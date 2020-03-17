37d898bd0575728acd069d75e4b7e65ea280f942
David Pawlowski <dpawlows@emich.edu>
2020-03-17 16:17:00 -0400

    enabled latex

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ html_logo = 'images/elogo1.png'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx',
+extensions = ['nbsphinx','sphinx.ext.mathjax'
 ]
 nbsphinx_execute = 'never'
 
@@ -41,7 +41,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+    '**.ipynb_checkpoints']
 html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
 
 #globaltoc makes it so sidebar doesn't change depending on page
